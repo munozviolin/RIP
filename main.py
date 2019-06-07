fedb44fc617560c1260a0a52280281011b9b4b77
jvs1991 <josue.valverde1@gmail.com>
2019-06-07 10:54:55 -0600

    Ciclo de servidor

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import threading
 import time
 import socket
 import struct
+import time
 
-UDP_IP = "127.168.0.0"
+UDP_IP = "127.168.0.1"
 UDP_PORT = 15005
 MESSAGE = "Hello, World!"
 TTL = struct.pack('b', 1)
 vecEntradas = {"127.168.0.1", "127.168.0.6", "127.168.0.10", "127.168.0.14", "127.168.0.17"}
 vecSalidas = {"127.168.0.2", "127.168.0.5", "127.168.0.9", "127.168.0.13", "127.168.0.18"}
-entrada = "127.168.0.1"
+#entrada = "127.168.0.1"
 barrier = threading.Barrier(3)#
 # contador = 0
 #nid0 = 100
@@ -18,25 +19,35 @@ barrier = threading.Barrier(3)#
 #nid3 = 103
 #nid4 = 104
 nid = [100, 101, 102, 103, 104]
-router1 = {1, "127.168.0.2"}
-router2 = {2, "127.168.0.18"}
-router3 = {3, "127.168.0.1", "127.168.0.6", "127.168.0.14", "127.168.0.17"}
-router4 = {4, "127.168.0.5", "127.168.0.10"}
-router5 = {5, "127.168.0.9", "127.168.0.13"}
+#router1 = {1, "127.168.0.2"}
+#router2 = {2, "127.168.0.18"}
+#router3 = {3, "127.168.0.1", "127.168.0.6", "127.168.0.14", "127.168.0.17"}
+#router4 = {4, "127.168.0.5", "127.168.0.10"}
+#router5 = {5, "127.168.0.9", "127.168.0.13"}
+listaIPCliente=["127.168.0.1", "127.168.0.2", "127.168.0.3", "127.168.0.4", "127.168.0.5"]
+router1 = {3}
+router2 = {3}
+router3 = {1,2,4,5}
+router4 = {3,5}
+router5 = {3,4}
 
+listasocket=[]
 
-def servidor(entrada,t):
+def servidor(t):
     #OTRA BARRERA VA ACÁ. 5 HILOS SE CREAN ACÁ
     #print("SERVER")
     #UDP_IP = entrada
     listaIPs = list(vecEntradas)
     listaIPs.extend(list(vecSalidas))
-    for x in listaIPs:
-        for i in range(4):
-            UDP_IP = x
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, TTL)
-            sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT + i))
+    global UDP_IP
+
+    global listasocket
+    for i in listasocket:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, TTL)
+        sock.sendto(MESSAGE.encode(), (UDP_IP, i))
+        #print(i)
+    #print("IP: ",x," port: ",UDP_PORT + i)
     #print("UDP target IP:", UDP_IP)
     #print("UDP target port:", UDP_PORT+t)
     #print("message:", MESSAGE)
@@ -44,33 +55,38 @@ def servidor(entrada,t):
 
 
 
-def cliente(entrada ,idRouter):
+def cliente(idRouter):
     # OTRA BARRERA VA ACÁ. 5 HILOS SE CREAN ACÁ.
     #print("CLIENT")
-    #print(UDP_PORT + idRouter)
-    UDP_IP = entrada
+    print("IPCLIENTE ",listaIPCliente[idRouter])
+    UDP_IP = listaIPCliente[idRouter]
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT + idRouter))
+    global listasocket
+    listasocket.append(UDP_PORT + idRouter)
     router={}
     if(idRouter == 0):
-        router = list(router1)
+        router = router1
     if (idRouter == 1):
-        router = list(router2)
+        router = router2
     if (idRouter == 2):
-        router = list(router3)
+        router = router3
     if (idRouter == 3):
-        router = list(router4)
+        router = router4
     if (idRouter == 4):
-        router = list(router5)
+        router = router5
 
     while True:
         data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
         direccion=list(sock.getsockname())
-        #print("received addr:", addr[0])
+        print("received addr: ", data)
         a=list(addr)
-        print (a[0])
+        print ("%%%% ",direccion[0])
         if( direccion[0] in router):
-            print("received message:", data)
+            print("received message:", data,"::::::::::",idRouter)
+        else:
+            print("========", idRouter)
+
     del router
 
 class threadC(threading.Thread):
@@ -78,7 +94,7 @@ class threadC(threading.Thread):
         threading.Thread.__init__(self)
         self.thread_ID = thread_ID
     def run(self):
-        cliente(entrada , self.thread_ID-100)
+        cliente( self.thread_ID-100)
         barrier.wait()
 
 class threadS(threading.Thread):
@@ -86,7 +102,17 @@ class threadS(threading.Thread):
         threading.Thread.__init__(self)
         self.thread_ID = thread_ID
     def run(self):
-        servidor(entrada, self.thread_ID-200)
+        servidor( self.thread_ID-200)
+
+def llamarServidor():
+    time.sleep(10)
+    listThread2 = []
+    for x in nid:
+        threadNID = threadS(x + 100)
+        listThread2.append(threadNID)
+    for x in listThread2:
+        x.start()
+
 
 # for i in vecEntradas:
 def main():
@@ -97,13 +123,8 @@ def main():
     for x in listThread:
         x.start()
        # barrier.wait()
-
-    listThread2 = []
-    for x in nid:
-        threadNID = threadS(x+100)
-        listThread2.append(threadNID)
-    for x in listThread2:
-        x.start()
+    while (True):
+       llamarServidor()
 
 
     print("Exit\n")
