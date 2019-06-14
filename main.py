8ba2eceff81571a6328b38b3f8f61c6d29a9d23b
munozviolin <yohelmr18@gmail.com>
2019-06-14 11:16:58 -0600

    contarSaltos

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import socket
 import struct
 import time
 
-thread3=[]
-stopthread=False
+contador = 1
 UDP_IP = "127.168.0.1"
 MASK = "255.255.255.0"
 UDP_PORT = 15005
@@ -33,6 +32,10 @@ tabla4 = [{"10.0.2.0", "255.255.255.0", "10.0.2.1", 1}]
 tabla5 = [{"10.0.4.0", "255.255.255.0", "10.0.4.1", 1}]
 listasocket = []
 caido = True
+redRouter1 = "10.0.1.0"
+redRouter2 = "10.0.3.0"
+redRouter4 = "10.0.2.0"
+redRouter5 = "10.0.4.0"
 
 def servidor(idRouter):
     idRouter += 1
@@ -55,7 +58,28 @@ def servidor(idRouter):
     # print("UDP target port:", UDP_PORT+t)
     # print("message:", MESSAGE)
 
-#def sumarSaltos():
+def contarSaltos(idRouterVecino, dirRed, saltos):
+    global redRouter1, redRouter2, redRouter4, redRouter5, router1, router2, router3, router4, router5
+
+    if (idRouterVecino == 1 and dirRed == redRouter1) or (idRouterVecino == 2 and dirRed == redRouter2) or (idRouterVecino == 4 and dirRed == redRouter4) or (idRouterVecino == 5 and dirRed == redRouter5):
+        saltos += 1
+    else:
+        saltos += 1
+        if idRouterVecino == 1:
+            for i in router1:
+                contarSaltos(i, dirRed, saltos)
+        elif idRouterVecino == 2:
+            for i in router2:
+                contarSaltos(i, dirRed, saltos)
+        elif idRouterVecino == 3:
+            for i in router3:
+                contarSaltos(i, dirRed, saltos)
+        elif idRouterVecino == 4:
+            for i in router4:
+                contarSaltos(i, dirRed, saltos)
+        elif idRouterVecino == 5:
+            for i in router5:
+                contarSaltos(i, dirRed, saltos)
 
 def cliente(idRouter):
     global tabla1
@@ -68,6 +92,8 @@ def cliente(idRouter):
     global router3IPs
     global router4IPs
     global router5IPs
+    global contador
+    saltos = 1
     dir1 = " "
     dir2 = " "
     dir3 = " "
@@ -80,7 +106,6 @@ def cliente(idRouter):
     sock.bind((UDP_IP, UDP_PORT + idRouter))
     global listasocket
     listasocket.append(UDP_PORT + idRouter)
-    salto = 1
 
     idRouter += 1
     router = {}
@@ -103,49 +128,48 @@ def cliente(idRouter):
         # print("%%%% ", direccion[0])
         mensaje = data.decode()
         if mensaje in router:
-            salto += 1
-            if idRouter == 3:
+            if idRouter == 3 and contador < 6:
                 print("SOY #3")
             #    print(tabla5)
                 #print("YO SOY: ", idRouter)
                 #print("escuché a: ", mensaje)
 
-            if mensaje == '1'and " " in router3IPs:
+            if mensaje == '1'and " " in router3IPs and contador < 6:
                 red = "10.0.1.0"
                 router3IPs[0] = red
                 siguiente = "192.168.0.1"
-                vec = {red, MASK, siguiente, salto}
+                saltos = 1
+                contarSaltos(mensaje, red, saltos)
+                vec = {red, MASK, siguiente, ' '}
                 tabla3.append(vec)
-            elif mensaje == '2' and " " in router3IPs:
+            elif mensaje == '2' and " " in router3IPs and contador < 6:
                 red = "10.0.3.0"
                 router3IPs[1] = red
                 siguiente = "192.168.0.17"
-                vec = {red, MASK, siguiente, salto}
+                vec = {red, MASK, siguiente, ' '}
                 tabla3.append(vec)
             elif mensaje == '4' :
                 red = "10.0.2.0"
-                if idRouter == 3 and " " in router3IPs:
+                if idRouter == 3 and " " in router3IPs and contador < 6:
                     siguiente = "192.168.0.6"
-                    vec = {red, MASK, siguiente, salto}
+                    vec = {red, MASK, siguiente, ' '}
                     tabla3.append(vec)
-
-
                     router3IPs[2] = red
                 elif idRouter == 5 and " " in router5IPs:
                     siguiente = "192.168.0.9"
-                    vec = {red, MASK, siguiente, salto}
+                    vec = {red, MASK, siguiente, ' '}
                     tabla5.append(vec)
                     router5IPs[2] = red
             elif mensaje == '5':
                 red = "10.0.4.0"
-                if idRouter == 3 and " " in router3IPs:
+                if idRouter == 3 and " " in router3IPs and contador < 6:
                     router3IPs[3] = red
                     siguiente = "192.168.0.14"
-                    vec = {red, MASK, siguiente, salto}
+                    vec = {red, MASK, siguiente, ' '}
                     tabla3.append(vec)
                 elif idRouter == 4 and " " in router4IPs:
                     siguiente = "192.168.0.10"
-                    vec = {red, MASK, siguiente, salto}
+                    vec = {red, MASK, siguiente, ' '}
                     tabla4.append(vec)
                     router4IPs[2] = red
             elif mensaje == '3':  # y comparar que router3ips.size sea menor que 4
@@ -155,21 +179,21 @@ def cliente(idRouter):
                         dir1 = router3IPs[1]
                         if dir1 != " " and " " in router1IPs:
                             red = dir1
-                            vec = {red, MASK, siguiente, salto}
+                            vec = {red, MASK, siguiente, ' '}
                             tabla1.append(vec)
                             router1IPs[0] = red
 
                         dir2 = router3IPs[2]
                         if dir2 != " "and " " in router1IPs:
                             red = dir2
-                            vec = {red, MASK, siguiente, salto}
+                            vec = {red, MASK, siguiente, ' '}
                             tabla1.append(vec)
                             router1IPs[1] = red
 
                         dir3 = router3IPs[3]
                         if dir3 != " "and " " in router1IPs:
                             red = dir3
-                            vec = {red, MASK, siguiente, salto}
+                            vec = {red, MASK, siguiente, ' '}
                             tabla1.append(vec)
                             router1IPs[2] = red
                     elif idRouter == 2:
@@ -177,21 +201,21 @@ def cliente(idRouter):
                         dir1 = router3IPs[0]
                         if dir1 != " "and " " in router2IPs:
                             red = dir1
-                            vec = {red, MASK, siguiente, salto}
+                            vec = {red, MASK, siguiente, ' '}
                             tabla2.append(vec)
                             router2IPs[0] = red
 
                         dir2 = router3IPs[2]
                         if dir2 != " "and " " in router2IPs:
                             red = dir2
-                            vec = {red, MASK, siguiente, salto}
+                            vec = {red, MASK, siguiente, ' '}
                             tabla2.append(vec)
                             router2IPs[1] = red
 
                         dir3 = router3IPs[3]
                         if dir3 != " "and " " in router2IPs:
                             red = dir3
-                            vec = {red, MASK, siguiente, salto}
+                            vec = {red, MASK, siguiente, ' '}
                             tabla2.append(vec)
                             router2IPs[2] = red
                     elif idRouter == 4:
@@ -199,14 +223,14 @@ def cliente(idRouter):
                         dir1 = router3IPs[0]
                         if dir1 != " "and " " in router4IPs:
                             red = dir1
-                            vec = {red, MASK, siguiente, salto}
+                            vec = {red, MASK, siguiente, ' '}
                             tabla4.append(vec)
                             router4IPs[0] = red
 
                         dir2 = router3IPs[1]
                         if dir2 != " "and " " in router4IPs:
                             red = dir2
-                            vec = {red, MASK, siguiente, salto}
+                            vec = {red, MASK, siguiente, ' '}
                             tabla4.append(vec)
                             router4IPs[1] = red
 
@@ -215,14 +239,14 @@ def cliente(idRouter):
                         dir1 = router3IPs[0]
                         if dir1 != " "and " " in router5IPs:
                             red = dir1
-                            vec = {red, MASK, siguiente, salto}
+                            vec = {red, MASK, siguiente, ' '}
                             tabla5.append(vec)
                             router5IPs[0] = red
 
                         dir2 = router3IPs[1]
                         if dir2 != " "and " " in router5IPs:
                             red = dir2
-                            vec = {red, MASK, siguiente, salto}
+                            vec = {red, MASK, siguiente, ' '}
                             tabla5.append(vec)
                             router5IPs[1] = red
 
@@ -240,10 +264,10 @@ class threadC(threading.Thread):
 
     def run(self):
         cliente(self.thread_ID - 100)
-        global stopthread
-        while True:
-            if stopthread:
-                break
+        ##global stopthread
+        #while True:
+        #    if stopthread:
+        #        break
         barrier.wait()
 
 
@@ -258,10 +282,11 @@ class threadS(threading.Thread):
 
 
 def llamarServidor():
-    contador=1
-    global thread3
+    #contador=1
+    #global thread3
+    global contador
     while (True):
-        i =0
+        i = 0
         global caido
         time.sleep(2)
         listThread2 = []
@@ -272,9 +297,9 @@ def llamarServidor():
             if contador >= 6:
                 if x.thread_ID !=202:
                     x.start()
-                else:
-                    thread3[0].join()
-                    stopthread=True
+                #else:
+                #    stopthread = True
+                #    thread3[0].join()
             elif contador < 6:
                 x.start()
             i += 1
@@ -290,9 +315,9 @@ def main():
     for x in nid:
         threadNID = threadC(x)
         listThread.append(threadNID)
-        if nid==102:
-            global thread3
-            thread3.append( threadNID)
+        #if nid==102:
+        #    global thread3
+        #    thread3.append(threadNID)
     for x in listThread:
         x.start()
     # barrier.wait()
